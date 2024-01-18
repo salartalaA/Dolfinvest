7d4f6dff24c1df9d12c501eb5ab48cc6368e6c9f
salartalaA <salarmhmdn@gmail.com>
2024-01-18 18:08:35 +0330

    For Practice

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const mobile_menu = document.querySelector(".mobile-nav");
 menu_btn.addEventListener("click", function () {
   menu_btn.classList.toggle("is-active");
   mobile_menu.classList.toggle("is-active");
+  menu_btn.classList.toggle("sticky");
 });
 
 buttons.forEach((button) => {
